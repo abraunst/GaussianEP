a98ff417d2ad5162604000d742ee589894ebfd80
Alfredo Braunstein <alfredo.braunstein@polito.it>
2019-02-18 17:38:06 +0100

    fix url

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,10 @@
 using Documenter, GaussianEP
 
+push!(LOAD_PATH,"../src/")
 makedocs(sitename="GaussianEP")
 deploydocs(
-	   repo = "github.com/abraunst/GaussianEP.jl.git",
+	   branch = "gh-pages",
+	   repo = "github.com/abraunst/GaussianEP.git",
+	   devurl = "dev",
+	   versions = ["stable" => "v^", "v#.#", devurl => devurl]
 )
